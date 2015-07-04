55f7a2b2db5466f6471d1206ff5a092616faae21
nicholasantonov <nicholasantonov@gmail.com>
2015-07-04 19:21:17 -0400

    Add comment delete button

--- src/comment.js
+++ src/comment.js
@@ -1,8 +1,14 @@
 import m from 'mithril';
 
 export default {
+  controller: function (ctrl) {
+    return {
+
+    };
+  },
   view: function (ctrl, args) {
     return m("blockquote", [
+      ((args.comment.c_id !== -1) ? m("button.btn.waves-effect.waves-light.red.right.tight[type='button']", {onclick: ctrl.deletePost}, ["", m("i.material-icons", "delete")]) : ""),
       args.comment.comment,
       m("br"),
       m("a.quote-by[title='Send a private message']", {onclick: () => { $('#message-modal').openModal()}}, args.comment.name)
