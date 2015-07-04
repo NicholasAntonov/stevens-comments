c40e269483bec5203e158149ff2e266f74c02ff9
nicholasantonov <nicholasantonov@gmail.com>
2015-07-04 19:29:09 -0400

    Add frontend code for showing comment delete button

--- src/comment.js
+++ src/comment.js
@@ -8,7 +8,7 @@ export default {
   },
   view: function (ctrl, args) {
     return m("blockquote", [
-      ((args.comment.c_id !== -1) ? m("button.btn.waves-effect.waves-light.red.right.tight[type='button']", {onclick: ctrl.deletePost}, ["", m("i.material-icons", "delete")]) : ""),
+      ((args.comment.u_id !== -1) ? m("button.btn.waves-effect.waves-light.red.right.tight[type='button']", {onclick: ctrl.deletePost}, ["", m("i.material-icons", "delete")]) : ""),
       args.comment.comment,
       m("br"),
       m("a.quote-by[title='Send a private message']", {onclick: () => { $('#message-modal').openModal()}}, args.comment.name)
